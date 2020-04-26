23f8dfb6a13d9879d88820009075c84ac2983323
Daniel Ortega <daniel.ortega.mx@gmail.com>
2020-04-26 17:25:22 -0500

    Detect win

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 15;
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -149,6 +149,7 @@ const goal = Bodies.rectangle(
   unitLength * .7,
   unitLength * .7,
   {
+    label: 'goal',
     isStatic: true
   }
 );
@@ -158,7 +159,10 @@ World.add(world, goal);
 const ball = Bodies.circle(
   unitLength / 2,
   unitLength / 2,
-  unitLength / 4
+  unitLength / 4,
+  {
+    label: 'ball'
+  }
 );
 World.add(world, ball);
 
@@ -176,4 +180,18 @@ document.addEventListener('keydown', event => {
   if (event.keyCode === 65) {
     Body.setVelocity(ball, {x: x - 5, y });
   }
+});
+
+// Win condition
+Events.on(engine, 'collisionStart' , (event) => {
+  event.pairs.forEach(collision => {
+    const labels = ['ball', 'goal'];
+
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      console.log("User won!!!");
+    }
+  });
 });
